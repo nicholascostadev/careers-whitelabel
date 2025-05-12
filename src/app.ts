85a96925f73136786dcd9066fd5a20c9770e5155
Nicholas Costa <nicholascostadev@gmail.com>
2025-05-12 17:57:00 -0300

    Initial commit

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import "dotenv/config";
+import fastify from "fastify";
+import { env } from "./lib/env";
+
+export const app = fastify({
+	logger: true,
+});
+
+app.get("/", async (_request, reply) => {
+	return reply.send({ message: "Hello World" });
+});
+
+app.listen({ port: env.PORT }, (error) => {
+	if (error) {
+		console.error(error);
+		process.exit(1);
+	}
+});
---
+++ src/lib/env.ts
@@ -0,0 +1,17 @@
+import z from "zod";
+
+const _env = z.object({
+  NODE_ENV: z.enum(["development", "production", "test"]).default("development"),
+  PORT: z.number().default(8080),
+  DATABASE_URL: z.string(),
+});
+
+const parsedEnv = _env.safeParse(process.env);
+
+if (!parsedEnv.success) {
+  console.error("Invalid environment variables", parsedEnv.error.format());
+  process.exit(1);
+}
+
+export const env = parsedEnv.data;
+
